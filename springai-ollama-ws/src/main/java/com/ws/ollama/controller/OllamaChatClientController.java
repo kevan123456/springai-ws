293b03c74b526605a15006a206481ab65cbc8220
wangshun <e-shun.wang3@zeekrlife.com>
2025-06-09 09:48:53 +0800

    test

--- springai-ollama-ws/src/main/java/com/ws/ollama/controller/OllamaChatClientController.java
+++ springai-ollama-ws/src/main/java/com/ws/ollama/controller/OllamaChatClientController.java
@@ -3,21 +3,26 @@ package com.ws.ollama.controller;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.image.ImageModel;
 import org.springframework.ai.image.ImagePrompt;
 import org.springframework.ai.image.ImageResponse;
+import org.springframework.ai.model.Media;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.ollama.api.OllamaModel;
 import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.List;
 
 /**
  * @author yunhua
@@ -33,6 +38,7 @@ public class OllamaChatClientController {
     private OllamaChatModel ollamaChatModel;
     @Autowired
     private ChatClient chatClient ;
+
 
 
     @GetMapping("/chat")
@@ -53,6 +59,17 @@ public class OllamaChatClientController {
                 .advisors(advisorSpec -> advisorSpec.param(AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY,100))
                 .call()
                 .content();
+    }
+
+    @GetMapping("/multimodality")
+    public String multimodality(@RequestParam(value = "message",defaultValue = "从图片中看到了什么？") String message) throws Exception{
+        ClassPathResource imageData = new ClassPathResource("/777.png") ;
+        UserMessage userMessage = new UserMessage(message,
+                List.of(new Media(MimeTypeUtils.IMAGE_PNG,imageData)));
+
+        ChatResponse response = ollamaChatModel.call(new Prompt(userMessage,
+                OllamaOptions.builder().withModel("bakllava").build()));
+        return response.getResult().getOutput().getContent() ;
     }
 
 }
